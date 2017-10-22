5959ea2f6646052043577b31dbfd67df3e3e3223
Vlad <Vlad@hotmail.co.nz>
2017-10-22 22:06:13 +1300

    added a couple model tests

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@booking = Booking.new(flight_id: 1)
+	end
+
+	test "flight_id should be present" do 
+		@booking = Booking.new(flight_id: "")
+		assert_not @booking.valid?
+	end  
+
 end
